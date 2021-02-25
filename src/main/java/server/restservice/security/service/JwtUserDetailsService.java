5d881559a0ee97abfabfb2bd23970d3e972bb586
Shauli Genish <gshauli99@gmail.com>
2021-02-25 04:34:36 +0200

    Added security and authentication support using JWT

---
+++ src/main/java/server/restservice/security/service/JwtUserDetailsService.java
@@ -0,0 +1,29 @@
+package server.restservice.security.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//TODO: implement username password mechanism
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private List<String> usernames = Arrays.asList("admin", "shauli", "test", "a");
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (this.usernames.contains(username)) {
+            return new User(username, "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+}
