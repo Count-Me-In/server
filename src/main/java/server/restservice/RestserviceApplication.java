75593d7a8d32a21b845474b36293fce35f930506
Shauli Genish <gshauli99@gmail.com>
2021-06-07 02:29:40 +0300

    Added auction scheduler

--- src/main/java/server/restservice/RestserviceApplication.java
+++ src/main/java/server/restservice/RestserviceApplication.java
@@ -2,7 +2,9 @@ package server.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RestserviceApplication {
 
---
+++ src/main/java/server/restservice/service/AuctionScheduler.java
@@ -0,0 +1,24 @@
+package server.restservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import server.restservice.repository.EmployeeRepository;
+
+@Service
+@AllArgsConstructor
+public class AuctionScheduler {
+
+    @Autowired
+    @Qualifier("repositoryImplementation")
+    private EmployeeRepository employeeRepository;
+
+    @Scheduled(cron = "0 0 20 * * 6")
+    public void scheduleFixedDelayTask() {
+        employeeRepository.execAuction();
+    }
+    
+}
